36ac50a1d34a20213b25cd704e5b84b60bcaf3d8
Flo0807 <flo.are0807@gmail.com>
2020-01-31 23:40:18 +0100

    better player movement

--- Player.gd
+++ Player.gd
@@ -1,22 +1,61 @@
 extends KinematicBody2D
 
-export(float) var gravity = 200.0
-export(float) var walk_speed = 200
-export(float) var jump_power = 400.0
+# https://godotengine.org/qa/25897/2d-platformer-how-to-control-jump-height-mario-like-jumps
 
+#Jump 
+export var fall_gravity_scale := 150.0
+export var low_jump_gravity_scale := 100.0
+export var jump_power := 500.0
+export var move_speed := 350.0
+var jump_released = false
+
+#Physics
 var velocity = Vector2()
+var earth_gravity = 9.807 # m/s^2
+export var gravity_scale := 100.0
+var on_floor = false
+
 
 func _physics_process(delta):
-	velocity.y += delta * gravity
+	velocity.x = 0
 	
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y -= jump_power
+	if Input.is_action_just_released("jump"):
+		jump_released = true
 
-	if Input.is_action_pressed("left"):
-		velocity.x = -walk_speed
-	elif Input.is_action_pressed("right"):
-		velocity.x =  walk_speed
-	else:
-		velocity.x = 0
+	#Applying gravity to player
+	velocity += Vector2.DOWN * earth_gravity * gravity_scale * delta
 
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	#Jump Physics
+	if velocity.y > 0: #Player is falling
+		#Falling action is faster than jumping action | Like in mario
+		#On falling we apply a second gravity to the player
+		#We apply ((gravity_scale + fall_gravity_scale) * earth_gravity) gravity on the player
+		velocity += Vector2.DOWN * earth_gravity * fall_gravity_scale * delta 
+
+	elif velocity.y < 0 && jump_released: #Player is jumping 
+		#Jump Height depends on how long you will hold key
+		#If we release the jump before reaching the max height 
+		#We apply ((gravity_scale + low_jump_gravity_scale) * earth_gravity) gravity on the player
+		#It result on a lower jump
+		velocity += Vector2.DOWN * earth_gravity * low_jump_gravity_scale * delta
+
+	if on_floor:
+		if Input.is_action_just_pressed("jump"): 
+			velocity = Vector2.UP * jump_power #Normal Jump action
+			jump_released = false
+	
+	var factor = 1
+	
+	if not (is_on_floor()):
+		factor = 0.75
+	
+	if (Input.is_action_pressed("right")):
+		velocity.x += move_speed * factor
+	
+	if (Input.is_action_pressed("left")):
+		velocity.x -= move_speed * factor
+
+	velocity = move_and_slide(velocity, Vector2.UP) 
+	
+	if is_on_floor(): on_floor = true
+	else: on_floor = false
