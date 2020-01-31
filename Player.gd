70126f7109428089ad23c4e9d54b7fbd481822a8
tufteddeer <tufteddeer@disroot.org>
2020-01-31 22:45:42 +0100

    remove unnecessary output

--- Player.gd
+++ Player.gd
@@ -8,12 +8,10 @@ var velocity = Vector2()
 
 func _physics_process(delta):
 	velocity.y += delta * gravity
-
-	print(is_on_floor())
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y -= jump_power
-		print("jump")
+
 	if Input.is_action_pressed("left"):
 		velocity.x = -walk_speed
 	elif Input.is_action_pressed("right"):
