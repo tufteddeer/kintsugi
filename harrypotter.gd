241e6eb04f0f20f4d28f6c973a83e83263936271
tufteddeer <tufteddeer@disroot.org>
2020-02-01 03:25:18 +0100

    even better cracking algorithms \(0.0)/

--- harrypotter.gd
+++ harrypotter.gd
@@ -1,28 +1,40 @@
 extends Polygon2D
 
+onready var line = $Line2D
+
 export (int) var h_point_count = 5
-export (float) var h_point_space = 100
-export (float) var y_baseline
+var width
+
+# distance between to vertical points
+export (float) var v_offset = 10
+
+export (int) var y_min_offset = -30
+export (int) var y_max_offset = 30
 
 var vertices = PoolVector2Array()
 
 func _ready():
+	line.visible = false
 	randomize()
-		
-	for i in range(1, h_point_count):
-		vertices.append(Vector2(100*i,100 + rand_range(-30,30)))
 	
-	vertices.append(Vector2(100*h_point_count, 150))
+	var line_start = line.points[0]
+	var line_end = line.points[1]
+
+	width = line_start.distance_to(line_end)
+	
+	var h_space = width / (h_point_count - 1)
+	
+	vertices.append(line_start)
+	
+	for i in range(1, h_point_count-1):
+		vertices.append(Vector2(h_space * i,0 + rand_range(y_min_offset, y_max_offset)))
+	
+	vertices.append(line_end)
 	
 	for j in range(vertices.size()-2, 0, -1):
 		var v = vertices[j]
-		vertices.append(Vector2(v.x, v.y+50))
+		vertices.append(Vector2(v.x, v.y + v_offset))
 	
 	set_polygon(vertices)
+	
 
-	print(vertices)
-	update()
-
-func _draw():
-	for v in vertices:
-		draw_circle(v, 10, color)
