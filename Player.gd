be8fbcff9642ce959d6d83936409c21245a594aa
Flo0807 <flo.are0807@gmail.com>
2020-02-01 11:48:52 +0100

    finish player animations

--- Player.gd
+++ Player.gd
@@ -18,7 +18,6 @@ var earth_gravity = 9.807 # m/s^2
 export var gravity_scale := 100.0
 var on_floor = false
 
-
 func _physics_process(delta):
 	velocity.x = 0
 	
@@ -28,12 +27,17 @@ func _physics_process(delta):
 	#Applying gravity to player
 	velocity += Vector2.DOWN * earth_gravity * gravity_scale * delta
 
+	var inAir = false
+
+	if (velocity.y != 0 and not is_on_floor()):
+		inAir = true
+
 	#Jump Physics
 	if velocity.y > 0: #Player is falling
 		#Falling action is faster than jumping action | Like in mario
 		#On falling we apply a second gravity to the player
 		#We apply ((gravity_scale + fall_gravity_scale) * earth_gravity) gravity on the player
-		velocity += Vector2.DOWN * earth_gravity * fall_gravity_scale * delta 
+		velocity += Vector2.DOWN * earth_gravity * fall_gravity_scale * delta
 
 	elif velocity.y < 0 && jump_released: #Player is jumping 
 		#Jump Height depends on how long you will hold key
@@ -41,12 +45,7 @@ func _physics_process(delta):
 		#We apply ((gravity_scale + low_jump_gravity_scale) * earth_gravity) gravity on the player
 		#It result on a lower jump
 		velocity += Vector2.DOWN * earth_gravity * low_jump_gravity_scale * delta
-
-	if on_floor:
-		if Input.is_action_just_pressed("jump"): 
-			velocity = Vector2.UP * jump_power #Normal Jump action
-			jump_released = false
-	
+		
 	var factor = 1
 	
 	if not (is_on_floor()):
@@ -54,14 +53,30 @@ func _physics_process(delta):
 	
 	if (Input.is_action_pressed("right")):
 		velocity.x += move_speed * factor
-		animationPlayer.play("moveRight")
+		
+		if not inAir:
+			animationPlayer.play("moveRight")
 	
 	if (Input.is_action_pressed("left")):
 		velocity.x -= move_speed * factor
-		animationPlayer.play("moveLeft")
+		
+		if not inAir:
+			animationPlayer.play("moveLeft")
 
-	if (is_on_floor() and velocity.x == 0):
+	if (is_on_floor() and velocity.x == 0 and not inAir):
 		animationPlayer.play("idle")
+	
+	if on_floor:
+		if Input.is_action_just_pressed("jump"): 
+			velocity = Vector2.UP * jump_power #Normal Jump action
+			jump_released = false
+			animationPlayer.play("jump")
+	
+	if inAir:
+		if (velocity.x > 0):
+			$character.flip_h = false
+		elif (velocity.x < 0):
+			$character.flip_h = true
 	
 	velocity = move_and_slide(velocity, Vector2.UP, true) 
 	
