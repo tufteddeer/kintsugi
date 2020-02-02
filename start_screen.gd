f747a0f1ace67a2086b9f97d879a1817fc1f5cab
Thilo <info@thilovoss.de>
2020-02-02 03:15:53 +0100

    i hope it works

--- start_screen.gd
+++ start_screen.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
 var played = false
+var played2 = false
+onready var player1 = $titleplayer
+onready var player2 = $AnimationPlayer
+
+
 
 func _process(delta):
+	
+	if (not played2):
+		player2.play("leertaste")
+		played2 = true
+		
 	if (Input.is_action_pressed("jump") and not played):
-		$AnimationPlayer.play("fade")
+		player1.play("title")
+		player2.stop()
+		$steuerung_leer.visible = not $steuerung_leer.visible
+		player1.queue("bewegungstasten")
 		played = true
 	
+
