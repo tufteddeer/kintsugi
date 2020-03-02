cdc42563db2c513569255f81095e614ad4c0a629
tufteddeer <tufteddeer@disroot.org>
2020-03-02 11:57:30 +0100

    add touch event handling for the start screen

--- start_screen.gd
+++ start_screen.gd
@@ -10,19 +10,27 @@ onready var player3 = $AnimationPlayer3
 
 # warning-ignore:unused_argument
 func _process(delta):
-	
+
 	if (not played2):
 		player2.play("leertaste")
 		played2 = true
-		
-	if (Input.is_action_pressed("jump") and not played):
-		player1.play("title")
-		player2.stop()
-		player3.play("bewegungstasten")
-		$steuerung_leer.visible = not $steuerung_leer.visible
-		played = true
-	
+
 func _on_titleplayer_animation_started(anim_name):
 	if anim_name == "title":
 		if (get_parent().get_node("Player") != null):
 			get_parent().get_node("Player").game_started = true
+
+func _input(event):
+	if not played:
+		if event is InputEventScreenTouch:
+			if event.pressed:
+				start_game()
+		if Input.is_action_pressed("jump"):
+			start_game()
+
+func start_game():
+	player1.play("title")
+	player2.stop()
+	player3.play("bewegungstasten")
+	$steuerung_leer.visible = not $steuerung_leer.visible
+	played = true
